f35d70a4ac66da9d58a0f58148d5d34c8c04214a
guangchuang yu <guangchuangyu@gamil.com>
2017-08-29 13:39:01 +0800

    clean up comments

--- R/accessor.R
+++ R/accessor.R
@@ -1,12 +1,3 @@
-## ' access diff slot
-## '
-## ' @param x SeqDiff object
-## ' @return data.frame
-## ' @examples
-## ' fas <- list.files(system.file("examples","GVariation", package="seqcombo"),
-## ' pattern="fas", full.names=TRUE)
-## ' x1 <- seqdiff(fas[1], reference=1)
-## ' diff(x1)
 ##' @method diff SeqDiff
 ##' @export
 diff.SeqDiff <- function(x, ...) {
